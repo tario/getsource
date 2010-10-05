839e77dcbff91e0a17a30fd2247c59191e4e1149
Dario <darios3@gmail.com>
2010-10-05 11:14:03 -0300

    added examples extracted from rallhook

---
+++ examples/test1/main.rb
@@ -0,0 +1,13 @@
+require "rubygems"
+require "getsource"
+require "source1.rb"
+require "source2.rb"
+
+x = X.new
+y = Y.new
+
+print "definition of method :foo in #{x}: ", x.method(:foo).body.file,"\n" # source1.rb
+print "definition of method :foo in #{y}: ", y.method(:foo).body.file,"\n" # source2.rb
+
+print "definition of method :foo of class X in object #{y}: ", y.method(X,:foo).body.file,"\n" # source1.rb
+print "definition of method :foo of class Y in object #{y}: ", y.method(Y,:foo).body.file,"\n" # source1.rb
