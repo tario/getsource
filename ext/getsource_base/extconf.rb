0acb60d320a97e43c98a682888c1d3e6df45c72b
Dario Seminara <dseminara@tbx.net.com>
2015-03-03 10:45:52 -0300

    fixed extension file for ruby 2.2 NOTE: C extension is NOT needed on ruby 2.2

--- ext/getsource_base/extconf.rb
+++ ext/getsource_base/extconf.rb
@@ -1,17 +1,16 @@
 require 'mkmf'
 dir_config('getsource_base')
-CONFIG['CC'] = 'gcc'
+MakeMakefile::CONFIG['CC'] = 'gcc'
 
-ruby_version = Config::CONFIG["ruby_version"]
+ruby_version = MakeMakefile::CONFIG["ruby_version"]
 ruby_version = ruby_version.split(".")[0..1].join(".")
 
 if ruby_version == "1.8"
 	$CFLAGS = $CFLAGS + " -DRUBY1_8"
 elsif ruby_version == "1.9"
 	$CFLAGS = $CFLAGS + " -DRUBY1_9"
-else
-	print "ERROR: unknown ruby version: #{ruby_version}\n"
-	print "try passing the rubyversion by argument (1.8 or 1.9)\n"
+elsif ruby_version == "2.2"
+	$CFLAGS = $CFLAGS + " -DRUBY2_2"
 end
 
 $CFLAGS = $CFLAGS  + " -o $@"
