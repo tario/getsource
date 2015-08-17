0b6cf07fb0146762d21838aecdd99a73f95b1b45
Arthur Karganyan <dondenegocios@gmail.com>
2015-08-17 12:21:08 +0300

    Compatibility with 1.8, 1.9, 2.0, 2.1 rubies

--- ext/getsource_base/extconf.rb
+++ ext/getsource_base/extconf.rb
@@ -1,8 +1,11 @@
 require 'mkmf'
 dir_config('getsource_base')
-MakeMakefile::CONFIG['CC'] = 'gcc'
 
-ruby_version = MakeMakefile::CONFIG["ruby_version"]
+config_module = %w(1.8, 1.9, 2.0, 2.1).include?(ruby_version) ? Config : MakeMakefile 
+
+config_module::CONFIG['CC'] = 'gcc'
+
+ruby_version = config_module::CONFIG["ruby_version"]
 ruby_version = ruby_version.split(".")[0..1].join(".")
 
 if ruby_version == "1.8"
